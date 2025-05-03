592b96378c3dbea8f1bb25deeae8ed1e73e7d8e3
durgaharish <durgaharish@outlook.com>
2025-05-03 11:52:20 -0700

    first file

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+
+
+master_tabs = st.tabs(["Fine Tuning","Communication Primitives"])
+
+with master_tabs[0]:
+    st.markdown("""
+
+   #### Model Abstractions 
+   - Fine Tuning 
+   - Knowledge Distillation 
+        """)
+    
+with master_tabs[1]:
+
+    st.markdown("""
+
+    1.	One to One Communication 
+2.	One to Many Communication
+a.	Scatter – Send a tensor to different parts of tensor 
+b.	Gather – Gather data 
+c.	Reduce – Similar to Gather, But averaging/Summing 
+d.	Broadcast – Send Identical copies to all other workers 
+3.	Many to Many Communication 
+a.	All Reduce – Reduce on 0, 
+b.	All Gather – Perform gather on all Workers 
+
+        """)
